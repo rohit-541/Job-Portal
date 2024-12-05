e034ceafc5cc29feba7806f20aaf5ac6bff5875e
rohit-541 <rohitkumar240iit@gmail.com>
2024-12-05 16:35:53 +0530

    added job add portal

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 //Importing Module
 import {express} from 'express'
+import {expresslayout} from 'ejs-layout';
 
 //Create server
 const app = express();
-
-//Setup the sessions
-
-//
-
+app.use(express.static('/public'));
+app.use(expresslayout);
 
 //use ejs and ejs-layouts as middle-ware
 app.set('view-engine','ejs');
 app.set('views','./view');
 
 //Set routes
-app.get('/',)
+app.get('/')
 
 
 export default app;
---
+++ public/js/job-add-js.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("Jobform");
+function addSkill(event) {
+  event.preventDefault();
+  const btn1 = document.createElement("button");
+  const skillCont = document.getElementById("SkillsRequired");
+  SkilsArray.push(skillCont.value);
+  btn1.innerHTML = skillCont.value + " X";
+  skillCont.value = "";
+  btn1.classList.add("btn", "btn-success", "m-3");
+  btn1.addEventListener("click", (event) => {
+    event.preventDefault();
+    event.currentTarget.remove();
+  });
+  getDiv.append(btn1);
+}
+const getDiv = document.querySelector("#skillcontainer");
+
+const SkilsArray = [];
+
+const addbtn = document.querySelector("#add");
+console.log(addbtn);
+
+addbtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  addSkill(event);
+});
+
+form.FormData.append("skills", SkilsArray);
