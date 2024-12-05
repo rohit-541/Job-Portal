4d7c2868d29b7f1a369b98c7ce0dfee265678db9
rohit-541 <rohitkumar240iit@gmail.com>
2024-12-05 08:53:39 +0530

    Frontend started

--- index.js
+++ index.js
@@ -3,3 +3,18 @@ import {express} from 'express'
 
 //Create server
 const app = express();
+
+//Setup the sessions
+
+//
+
+
+//use ejs and ejs-layouts as middle-ware
+app.set('view-engine','ejs');
+app.set('views','./view');
+
+//Set routes
+app.get('/',)
+
+
+export default app;
